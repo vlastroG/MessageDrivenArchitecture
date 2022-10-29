38c7c8bd05d3c1ced6adfc748b92215401b2ea35
Vladimir Stroganov <vlastroganov@gmail.com>
2022-10-29 19:19:22 +0300

    Добавить Restaurant.Notificator

---
+++ Restaurant.Notification/Program.cs
@@ -0,0 +1,20 @@
+using Restaurant.Notification.Services.Background;
+
+namespace Restaurant.Notification
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureServices((hostContext, services) =>
+                {
+                    services.AddHostedService<Worker>();
+                });
+    }
+}
