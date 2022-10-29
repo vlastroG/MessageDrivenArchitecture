7c5d0d07b3c605bb703032427ca3a4ecef36e792
Vladimir Stroganov <vlastroganov@gmail.com>
2022-10-29 18:27:38 +0300

    Создать библиотеку Messaging

---
+++ Messaging/Consumer.cs
@@ -0,0 +1,63 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Messaging
+{
+    public class Consumer : IDisposable
+    {
+        private readonly string _queueName;
+        private readonly string _hostName;
+
+        private readonly IConnection _connection;
+        private readonly IModel _channel;
+
+        public Consumer(string queueName, string hostName)
+        {
+            _queueName = queueName;
+            _hostName = "shark.rmq.cloudamqp.com";
+            var factory = new ConnectionFactory()
+            {
+                HostName = _hostName,
+                Port = 5672,
+                UserName = "ueiosuvi",
+                Password = "KdYyQ2jvIP7hVpOP1IZLEyQkrRPI8MW8",
+                VirtualHost = "ueiosuvi"
+
+            };
+            _connection = factory.CreateConnection();
+            _channel = _connection.CreateModel();
+        }
+
+        public void Receive(EventHandler<BasicDeliverEventArgs> receiveCallback)
+        {
+            _channel.ExchangeDeclare(exchange: "direct_exchange",
+                type: "direct");
+
+            _channel.QueueDeclare(queue: _queueName,
+                durable: false,
+                exclusive: false,
+                autoDelete: false,
+                arguments: null);
+
+            _channel.QueueBind(queue: _queueName,
+                exchange: "direct_exchange",
+                routingKey: _queueName);
+
+            var consumer = new EventingBasicConsumer(_channel);
+            consumer.Received += receiveCallback;
+
+            _channel.BasicConsume(queue: _queueName, autoAck: true, consumer: consumer);
+        }
+
+        public void Dispose()
+        {
+            _connection?.Dispose();
+            _channel?.Dispose();
+        }
+    }
+}
---
+++ Messaging/Producer.cs
@@ -0,0 +1,51 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Messaging
+{
+    public class Producer
+    {
+        private readonly string _queueName;
+        private readonly string _hostName;
+
+        public Producer(string queueName, string hostName)
+        {
+            _queueName = queueName;
+            _hostName = "shark.rmq.cloudamqp.com";
+        }
+
+        public void Send(string message)
+        {
+            var factory = new ConnectionFactory()
+            {
+                HostName = _hostName,
+                Port = 5672,
+                UserName = "ueiosuvi",
+                Password = "KdYyQ2jvIP7hVpOP1IZLEyQkrRPI8MW8",
+                VirtualHost = "ueiosuvi"
+
+            };
+            using var connection = factory.CreateConnection();
+            using var channel = connection.CreateModel();
+
+            channel.ExchangeDeclare(
+                "direct_exchange",
+                "direct",
+                false,
+                false,
+                null
+            );
+
+            var body = Encoding.UTF8.GetBytes(message);
+
+            channel.BasicPublish(exchange: "direct_exchange",
+                routingKey: _queueName,
+                basicProperties: null,
+                body: body);
+        }
+    }
+}
