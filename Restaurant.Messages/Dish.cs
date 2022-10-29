312f56e13916d1b5b67c325a0e8b65503a7ff71e
Vladimir Stroganov <vlastroganov@gmail.com>
2022-10-29 22:27:05 +0300

    Создать библиотеку Restaurant.Messages

---
+++ Restaurant.Messages/Dish.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Restaurant.Messages
+{
+    /// <summary>
+    /// Перечисление доступных блюд
+    /// </summary>
+    public enum Dish
+    {
+        /// <summary>
+        /// Курица
+        /// </summary>
+        Chicken = 0,
+
+        /// <summary>
+        /// Пицца
+        /// </summary>
+        Pizza = 1,
+
+        /// <summary>
+        /// Паста
+        /// </summary>
+        Pasta = 2,
+
+        /// <summary>
+        /// Лазанья
+        /// </summary>
+        Lasagna = 3
+    }
+}
