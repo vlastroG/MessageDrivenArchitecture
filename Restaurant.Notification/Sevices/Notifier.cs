f906cfb680f1c777f014a2c01c9715fc386fd42a
Vladimir Stroganov <vlastroganov@gmail.com>
2022-10-29 22:43:48 +0300

    Скорректировать проект Restaurant.Notification

---
+++ Restaurant.Notification/Sevices/Notifier.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Concurrent;
+
+namespace Restaurant.Notification.Sevices
+{
+    public class Notifier
+    {
+        /// <summary>
+        /// Кэш для хранения статусов, номера заказа и клиента
+        /// </summary>
+        private readonly ConcurrentDictionary<Guid, Tuple<Guid?, Accepted>> _state = new();
+
+        public void Accept(Guid orderId, Accepted accepted, Guid? clientId = null)
+        {
+            _state.AddOrUpdate(orderId, new Tuple<Guid?, Accepted>(clientId, accepted),
+                (guid, oldValue) => new Tuple<Guid?, Accepted>(
+                    oldValue.Item1 ?? clientId, oldValue.Item2 | accepted));
+
+            Notify(orderId);
+        }
+
+        private void Notify(Guid orderId)
+        {
+            var booking = _state[orderId];
+
+            switch (booking.Item2)
+            {
+                case Accepted.All:
+                    Console.WriteLine($"Успешно забронировано для клиента {booking.Item1}");
+                    _state.Remove(orderId, out _);
+                    break;
+                case Accepted.Rejected:
+                    Console.WriteLine($"Гость {booking.Item1}, к сожалению, все столики заняты");
+                    _state.Remove(orderId, out _);
+                    break;
+                case Accepted.Kitchen:
+                case Accepted.Booking:
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+        }
+    }
+}
