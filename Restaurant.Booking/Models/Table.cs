127d071fa6ae94c9a7d586c3d20de2f2e3da2a6a
Vladimir Stroganov <vlastroganov@gmail.com>
2022-10-29 18:48:59 +0300

    Добавить проект Restaurant.Booking

---
+++ Restaurant.Booking/Models/Table.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Restaurant.Booking.Models
+{
+    public class Table
+    {
+        public State State { get; private set; }
+
+        public int SeatsCount { get; }
+
+        public int Id { get; }
+
+        private readonly object _lock = new object();
+
+        /// <summary>
+        /// Конструктор стола по id
+        /// </summary>
+        /// <param name="id">Идентификационный номер</param>
+        public Table(int id) : this(id, Random.Shared.Next(1, 13)) { }
+
+        /// <summary>
+        /// Конструктор стола по id и количеству мест
+        /// </summary>
+        /// <param name="id">Идентификационный номер</param>
+        /// <param name="seatsCount">Количество мест должно быть от 1 до 12 включая концы</param>
+        /// <exception cref="ArgumentException">Исключение, если количество мест некорректно</exception>
+        public Table(int id, int seatsCount)
+        {
+            Id = id;
+            if (id < 1)
+            {
+                throw new ArgumentException($"Id должно быть не менее 1, получено: {id}");
+            }
+            if (seatsCount < 1 || seatsCount > 12)
+                throw new ArgumentException($"У стола может быть от 1 до 12 мест, получено: {seatsCount}");
+            SeatsCount = seatsCount;
+            State = State.Free;
+        }
+
+        /// <summary>
+        /// Устанавливает столу новое состояние
+        /// </summary>
+        /// <param name="state"></param>
+        /// <returns>True, если состояние изменено, иначе false</returns>
+        public bool SetState(State state)
+        {
+            lock (_lock)
+            {
+                if (state == State) return false;
+                State = state;
+                return true;
+            }
+        }
+    }
+}
---
+++ Restaurant.Booking/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Diagnostics;
+
+namespace Restaurant.Booking
+{
+    internal static class Program
+    {
+        private static async Task Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            var rest = new Restaurant.Booking.Models.Restaurant();
+            while (true)
+            {
+                await Task.Delay(10000);
+
+                Console.WriteLine("Привет! Желаете забронировать столик?");
+
+                var stopWatch = new Stopwatch();
+
+                stopWatch.Start();
+                rest.BookTableAsync(1);
+                stopWatch.Stop();
+
+                Console.WriteLine("Спасибо за Ваше обращение!");
+                var ts = stopWatch.Elapsed;
+                Console.WriteLine($"{ts.Seconds:00}:{ts.Milliseconds:00}");
+            }
+        }
+    }
+}
