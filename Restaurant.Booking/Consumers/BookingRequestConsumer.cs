cb6db35692a9db902f259d4f35f77519869e8daf
Vladimir Stroganov <vlastroganov@gmail.com>
2022-11-04 14:28:51 +0300

    Добавить consumers в Restaurant.Booking

---
+++ Restaurant.Booking/Consumers/BookingRequestConsumer.cs
@@ -0,0 +1,29 @@
+﻿using MassTransit;
+using Restaurant.Messages.Implements;
+using Restaurant.Messages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Restaurant.Booking.Consumers
+{
+    public class RestaurantBookingRequestConsumer : IConsumer<IBookingRequest>
+    {
+        private readonly Restaurant.Booking.Models.Restaurant _restaurant;
+
+        public RestaurantBookingRequestConsumer(Restaurant.Booking.Models.Restaurant restaurant)
+        {
+            _restaurant = restaurant;
+        }
+
+        public async Task Consume(ConsumeContext<IBookingRequest> context)
+        {
+            Console.WriteLine($"[OrderId: {context.Message.OrderId}]");
+            var result = await _restaurant.BookTableAsync(1);
+
+            await context.Publish<ITableBooked>(new TableBooked(context.Message.OrderId, result ?? false, Dish.Pizza));
+        }
+    }
+}
