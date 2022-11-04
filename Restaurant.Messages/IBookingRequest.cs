faf0b02c6c384a1c9f1357b9c3dc4e797cb0d18e
Vladimir Stroganov <vlastroganov@gmail.com>
2022-11-04 14:28:24 +0300

    Добавить классы сообщений в Messages

---
+++ Restaurant.Messages/IBookingRequest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Restaurant.Messages
+{
+    public interface IBookingRequest
+    {
+        public Guid OrderId { get; }
+
+        public Guid ClientId { get; }
+
+        public Dish? PreOrder { get; }
+
+        public DateTime CreationDate { get; }
+    }
+}
---
+++ Restaurant.Messages/Implements/BookingCancellation.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Restaurant.Messages.Implements
+{
+    public class BookingCancellation : IBookingCancellation
+    {
+        public BookingCancellation(Guid orderId)
+        {
+            OrderId = orderId;
+        }
+
+        public Guid OrderId { get; }
+    }
+}
