fa83529594c9a64ad4310e1871ef2fd055c3dbdb
Vladimir Stroganov <vlastroganov@gmail.com>
2022-11-18 21:23:51 +0300

    Добавить MemoryRepository

---
+++ Restaurant.Messages/MemoryDb/IMemoryRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Restaurant.Messages.MemoryDb
+{
+    public interface IMemoryRepository<T> where T : class
+    {
+        public void AddOrUpdate(T entity);
+
+        public IEnumerable<T> Get();
+    }
+}
